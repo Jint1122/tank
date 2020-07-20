10c25e835cafa5fb2fc9e0b45c7c08e85a6a0790
Jint1122 <1185708632@qq.com>
2020-07-20 23:47:48 +0800

    添加图片测试类

---
+++ src/com/jint/tank/test/ImageTest.java
@@ -0,0 +1,24 @@
+package com.jint.tank.test;
+
+import org.junit.jupiter.api.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * Created by jint on 2020/7/20.
+ */
+public class ImageTest {
+    @Test
+    void test() {
+        try {
+            BufferedImage bufferedImage = ImageIO.read(ImageTest.class.getClassLoader().getResourceAsStream("image/bulletD.gif"));
+            assertNotNull(bufferedImage);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
